5b578e5cdc51809107462161ca9d55bc0b9b5c40
Phil Rosegay <prosegay@voltdb.com>
2019-01-27 16:37:16 -0500

    update db connection to new db

--- src/main/java/hudson/tasks/junit/TestResult.java
+++ src/main/java/hudson/tasks/junit/TestResult.java
@@ -131,7 +131,7 @@ public final class TestResult extends MetaTabulatedResult {
     String returnString = new String();
     String username = "junit-plugin";
     String password = "TVPqeaGmk3GzeKb2";
-    String connectionUrl = "jdbc:mysql://volt2.voltdb.lan/qa";
+    String connectionUrl = "jdbc:mysql://junitstatsdb.voltdb.lan/qa";
     String testNameReformat = testName.substring(0, testName.length() - 1);
     testNameReformat = testNameReformat.replace("/t",".t").replace("/T",".T").replace("_","-");
     try {
